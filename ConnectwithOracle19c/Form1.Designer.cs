059db8391395bd70886ab0327653e68470ff85f9
gohul <gohul@DESKTOP-E19DP1F>
2022-11-23 23:20:26 +0530

    Add project files.

---
+++ ConnectwithOracle19c/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ConnectwithOracle19c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Retrieve Results";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result will display Here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(168, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Connect DotNet with Oracle 19c";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 336);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Connect Oracle 19c with DotNet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConnectwithOracle19c/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ConnectwithOracle19c
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+
+            string connectionstring = "Data Source = (DESCRIPTION = " +
+             "(ADDRESS = (PROTOCOL = TCP)(HOST = YOUR ORACLE DATABASE SERVER IP/SERVER NAME HERE)(PORT = 1521))" +
+             "(CONNECT_DATA = " +
+               " (SERVER = DEDICATED)" +
+               " (SERVICE_NAME = ORACLE DATABASE ID HERE)" +
+             " )" +
+           "); User Id = ORACLE DATABASE USER HERE ; password=ORACLE DATABASE PASSWORD HERE;";
+
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = connectionstring;
+            con.Open();
+
+            //OracleCommand oraCmd = new OracleCommand("DELETE FROM DEMODATA WHERE FNAME='HARRY'", conn);
+            OracleCommand cmd = new OracleCommand();
+            cmd.CommandText = "select username from Employee where employeeid=1 ";
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+            MessageBox.Show(dr.GetString(0));
+            label1.Text = dr.GetString(0);
+
+        } }
+    }
+
